78bc12e9dee672b725119f9d9356456b4ecc8dd3
Garrett Jones <garretcj2010@hotmail.com>
2023-07-30 16:15:21 -0500

    Added pytests for workflow

--- src/main.py
+++ src/main.py
@@ -3,6 +3,13 @@
 def world():
     print("world from jenkins")
 
+def inc(x: int):
+    return x + 1
+
+def test_world():
+    assert inc(1) == 2
+
+
 
 if __name__ == "__main__":
     print("hello")
---
+++ src/test/test_main.py
@@ -0,0 +1,8 @@
+
+
+def inc(x: int):
+    return x + 1
+
+def test_world():
+    assert inc(1) == 2
+
