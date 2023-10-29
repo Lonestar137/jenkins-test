64b0b83692c17d12c54e91b845fcaca7e4d35824
Garrett Jones <garretcj2010@hotmail.com>
2023-10-29 13:34:52 -0500

    add build test

---
+++ build_test/build.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+from build_tools.context.venv import Venv
+from build_tools.context.context_dir import Dir
+from build_tools.cmd.git import GitRepo, PythonGitRepo
+
+
+# def stage(f, name: str):
+#     def wrapper():
+#         pass
+
+#     return wrapper
+
+
+# @stage("Build Stage")
+def build():
+    requirements = ["pytest"]
+    with Dir("test"):
+        with Venv("venv", py_modules=requirements):
+            GitRepo("https://github.com/Lonestar137/JustNeovim.git").clone()
+            sample = PythonGitRepo(
+                "https://github.com/pypa/sampleproject").clone()
+            sample.wheels("wheelhouse")
+            with Dir("sampleproject"):
+                subprocess.run("pytest")
+                print("Curdir: ", os.getcwd())
+
+
+if __name__ == "__main__":
+    build()
